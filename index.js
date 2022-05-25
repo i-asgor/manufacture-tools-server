81c8c07bb23f651cef24ec7c328e263ae7bfe06b
Asgor Ali <i.joni40@gmail.com>
2022-05-26 02:22:52 +0600

    create payment console stop

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ async function run(){
           currency: 'usd',
           payment_method_types:['card']
         });
-        console.log(paymentIntent)
+        // console.log(paymentIntent)
         res.send({clientSecret: paymentIntent.client_secret})
       })
 
